8f6efedc1494b351094489ed219a60688e8c7192
jeongsh924 <jeong.sh924@gmail.com>
2025-10-16 23:25:02 +0900

    content 작업 : about, skills, projects

---
+++ js/dist/theme.dev.js
@@ -0,0 +1,16 @@
+"use strict";
+
+$(function () {
+  var currentTheme = 'vs2015';
+  $('#switch-theme').on('click', function () {
+    var themeLink = $('#hljs-theme');
+
+    if (currentTheme === 'vs2015') {
+      themeLink.attr('href', 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/styles/github.min.css');
+      currentTheme = 'github';
+    } else {
+      themeLink.attr('href', 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/styles/vs2015.min.css');
+      currentTheme = 'vs2015';
+    }
+  });
+});
